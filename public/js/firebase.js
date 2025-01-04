2450641971193f458c398290efb4e075156bd613
ritz0007 <64972613+ritz0007@users.noreply.github.com>
2025-01-04 15:31:19 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+  apiKey: "AIzaSyCnV7ansn1Ewp_p7WNT8SfrSywrRYiiitI",
+  authDomain: "blog-db-48d42.firebaseapp.com",
+  projectId: "blog-db-48d42",
+  storageBucket: "blog-db-48d42.firebasestorage.app",
+  messagingSenderId: "162503675610",
+  appId: "1:162503675610:web:e6d2746a4a3d627c36dfe3",
+  measurementId: "G-9DS6SNDVWT"
 };
 
 firebase.initializeApp(firebaseConfig);
